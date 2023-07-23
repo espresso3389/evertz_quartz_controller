a10d0aa710acd9466ea27d542cc0e2e60b33f086
Takashi Kawasaki <espresso3389@gmail.com>
2023-07-23 10:29:00 +0900

    WIP

--- bin/evertz_quartz_controller.dart
+++ bin/evertz_quartz_controller.dart
@@ -1,14 +1,6 @@
 import 'package:evertz_quartz_controller/evertz_quartz_controller.dart';
 
 void main(List<String> arguments) async {
-  if (arguments.length != 2) {
-    print('test DEST DURATION_MILLISECONDS');
-    return;
-  }
-  final dest = int.parse(arguments[0]);
-  final interval = Duration(milliseconds: int.parse(arguments[1]));
-  print('dest=$dest, interval=$interval');
-
   final launchAt = DateTime.now();
   final ec = EvertController(address: '192.168.10.34', port: 3739);
   DateTime lastHeartbeat = DateTime.now();
@@ -19,30 +11,20 @@ void main(List<String> arguments) async {
     lastHeartbeat = DateTime.now();
   });
 
-  test(ec, dest: dest, interval: interval);
-
-  while (true) {
-    final now = DateTime.now();
-    if (now.difference(lastHeartbeat) > Duration(seconds: 80)) {
-      print('''************************************
-EVERTZ FAILURE on $now (${now.difference(launchAt)})
-************************************''');
-      return;
-    }
-    await Future.delayed(Duration(seconds: 1));
-  }
-
   // for (int i = 49; i <= 64; i++) {
   //   await ec.send('.SV$i,$i\r');
   // }
 
-  // for (int i = 49; i <= 64; i++) {
-  //   await ec.send('.IV$i\r');
-  // }
+  for (int i = 1; i <= 64; i++) {
+    await ec.send('.IV$i\r');
+  }
 
-  // await ec.send('.SV${arguments[0]},${arguments[1]}\r');
-  // print('${arguments[0]} -> ${arguments[1]}');
-  // await ec.send('.IV${arguments[0]}\r');
+  const dest = 58; // 58=MONI
+  const src = 35; // 33=STB,34=OtherPC1,...
+
+  await ec.send('.SV$dest,$src\r');
+  print('$dest -> $src');
+  await ec.send('.IV$dest\r');
 }
 
 Future<void> test(
--- example2/android/app/src/main/kotlin/jp/espresso3389/evertzquartz/example2/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package jp.espresso3389.evertzquartz.example2
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
